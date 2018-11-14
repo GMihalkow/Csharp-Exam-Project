909c2bb12ebf4e7bee8e022f44f6e4c94c4d9235
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2018-11-14 23:29:06 +0200

    Implemented db layer.

---
+++ Forum/Forum.Data/ForumDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Forum.Data.EntityConfiguration;
+using Forum.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forum.Data
+{
+    public class ForumDbContext : IdentityDbContext<ForumUser>
+    {
+        public ForumDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Categorie> Categories { get; set; }
+
+        public DbSet<SubForum> Forums { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+
+        public DbSet<Quote> Quotes { get; set; }
+
+        public DbSet<Reply> Replies { get; set; }
+
+        public DbSet<Report> Reports { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=ForumDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration<Report>(new ReportConfiguration());
+            builder.ApplyConfiguration<Quote>(new QuoteConfiguration());
+            builder.ApplyConfiguration<Post>(new PostConfiguration());
+            builder.ApplyConfiguration<Reply>(new ReplyConfiguration());
+            builder.ApplyConfiguration<Categorie>(new CategorieConfiguration());
+        }
+    }
+}
---
+++ Forum/Forum.Models/Report.cs
@@ -0,0 +1,19 @@
+﻿namespace Forum.Models
+{
+    using System;
+
+    public class Report
+    {
+        public string Id { get; set; }
+
+        public ForumUser User { get; set; }
+
+        public string UserId { get; set; }
+
+        public Post Post { get; set; }
+
+        public string PostId { get; set; }
+
+        public DateTime OpenedOn { get; set; }
+    }
+}
