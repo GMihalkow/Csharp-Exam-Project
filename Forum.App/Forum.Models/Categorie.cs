f2d2e85cb7ad092b9c0598ae7acbafe7797d4da6
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2018-11-14 23:31:18 +0200

    Implemented db layer.

---
+++ Forum.App/Forum.Models/Categorie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Forum.Models
+{
+    public class Categorie
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<SubForum> Forums { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+    }
+}
---
+++ Forum.App/Forum.Models/Quote.cs
@@ -0,0 +1,21 @@
+﻿namespace Forum.Models
+{
+    public class Quote
+    {
+        public string Id { get; set; }
+
+        public Reply Reply { get; set; }
+
+        public string ReplyId { get; set; }
+
+        public ForumUser Author { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string Descrption { get; set; }
+
+        public ForumUser Reciever { get; set; }
+
+        public string RecieverId { get; set; }
+    }
+}
