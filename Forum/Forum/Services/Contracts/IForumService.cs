d1bd8239a6dab443e0e133f39b0ecfbb9cef3f2b
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2018-11-27 01:12:16 +0200

    Added Forum Service interface

---
+++ Forum/Forum/Services/Contracts/IForumService.cs
@@ -0,0 +1,13 @@
+﻿namespace Forum.Web.Services.Contracts
+{
+    using Forum.Models;
+    using Forum.Web.ViewModels.Forum;
+    using System.Collections.Generic;
+
+    public interface IForumService
+    {
+        void Add(ForumFormInputModel model);
+
+        ICollection<Post> GetPostsByForum(string id);
+    }
+}
