64d6e795b6ec00db62cc088ce5a650cfd4916da9
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2018-11-27 15:39:25 +0200

    Added Forum controller.

---
+++ Forum/Forum/Controllers/Forum/ForumController.cs
@@ -0,0 +1,56 @@
+﻿namespace Forum.Web.Controllers.Forum
+{
+    using global::Forum.Web.Services.Contracts;
+    using global::Forum.Web.ViewModels.Forum;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize("Admin")]
+    public class ForumController : BaseController
+    {
+        private readonly ICategoryService categoryService;
+        private readonly IForumService forumService;
+
+        public ForumController(IAccountService accountService, ICategoryService categoryService, IForumService forumService)
+            : base(accountService)
+        {
+            this.categoryService = categoryService;
+            this.forumService = forumService;
+        }
+
+        public IActionResult Create()
+        {
+            var names = this.categoryService.GetCategoriesNames();
+
+            ForumFormInputModel model = new ForumFormInputModel
+            {
+                Categories = names
+            };
+            
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(ForumFormInputModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                this.forumService.Add(model);
+
+                return this.Redirect("/");
+            }
+            else
+            {
+                return this.View(model);
+            }
+        }
+
+        [AllowAnonymous]
+        public IActionResult Posts(string id)
+        {
+            var model = this.forumService.GetPostsByForum(id);
+
+            return this.View(model);
+        }
+    }
+}
